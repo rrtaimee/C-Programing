34196b4fc5b3b9f6214ec287a8d8966a90a3efab
Taimee <34535959+rr-taimee@users.noreply.github.com>
2023-11-28 08:46:30 +0600

    Add files via upload

---
+++ Escape sequence.c
@@ -0,0 +1,10 @@
+ #include<stdio.h>
+int main()
+{
+    printf("Name: \nTaimee \noccupation: \nStudent"); //use of \n for new line
+    printf("\nA\tB\tC\t"); //use of \t for tab
+    printf("\n\'Hi! how are you?\'"); //use of /' to print single quote
+    printf("\n\"HI! how are you?\""); //use of \" to point double quote
+    return 0;
+
+}
